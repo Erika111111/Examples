c2fbc6109ee320f8323d2eb0a28560a76fac9d8a
Erika <Erika1111@yandex.ru>
2022-12-07 15:01:11 +0300

     Example 010

---
+++ Example010/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1, 28, 34, 41, 15, 67, 17, 8} ;
+
+int n = array.Length;
+int find = 67;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1;
+    index++;
+}
